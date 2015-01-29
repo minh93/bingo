466d0e60f363aa75d67163b36fe71bd52f4d0b2e
kienphan <senior1206@gmail.com>
2015-01-30 00:24:38 +0700

    try fix qr_code bug

--- app/controllers/qrcode_controller.rb
+++ app/controllers/qrcode_controller.rb
@@ -3,16 +3,7 @@ require 'rqrcode_png'
 class QrcodeController < ApplicationController
   def index
   	@host = request.host_with_port
-    #@qr = RQRCode::QRCode.new(@host + '/player/login', :level => :h).to_img.resize(200, 200).to_data_url
-    qr_size = 3
-    @qr = nil
-    while @qr == nil && qr_size < 10
-      begin
-        @qr = RQRCode::QRCode.new(@host + '/player/login', :size => qr_size, :level => :l)
-      rescue RQRCode::QRCodeRunTimeError => e
-        qr_size += 1
-      end
-    end
+    @qr = RQRCode::QRCode.new(@host + '/player/login', :size => 14, :level => :h).to_img.resize(200, 200).to_data_url
   end
-  
+
 end
