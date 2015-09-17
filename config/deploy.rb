655354b787265a10ec421ea5461d9a1b5611d77e
minh93 <phamducminh.bri@gmail.com>
2015-09-17 11:05:57 +0700

    fix path puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,10 +16,10 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_state,      "deploy/#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "deploy/#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "deploy/#{release_path}/log/puma.error.log"
+set :puma_error_log,  "deploy/#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
@@ -40,8 +40,8 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+      execute "mkdir deploy/#{shared_path}/tmp/sockets -p"
+      execute "mkdir deploy/#{shared_path}/tmp/pids -p"
     end
   end
 
