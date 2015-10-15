78d7dc4db5c36044dd352495b28ac960a13bf113
minh93 <phamducminh.bri@gmail.com>
2015-10-15 17:04:31 +0700

    fix postgres string type 255 chars

--- db/migrate/20151015083509_add_not_exist_deal_and_turn_to_deals.rb
+++ db/migrate/20151015083509_add_not_exist_deal_and_turn_to_deals.rb
@@ -1,6 +1,6 @@
 class AddNotExistDealAndTurnToDeals < ActiveRecord::Migration
   def change
-    add_column :deals, :not_exist_deal, :string
+    add_column :deals, :not_exist_deal, :text
     add_column :deals, :turn, :integer
   end
 end
